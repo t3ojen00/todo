cb6e6dd0eff71989f0d10671988695d836c774b8
enniemilia <130854866+enniemilia@users.noreply.github.com>
2024-11-10 21:30:56 +0200

    apuaaaaaa

--- server/index.test.js
+++ server/index.test.js
@@ -89,21 +89,5 @@ describe("POST register", () => {
       expect(data).to.be.an("object");
       expect(data).to.include.all.keys("id", "email");
     });
-  
-    /*it("should not register with less than 8 character password", async () => {
-      const email = "register@foo.com";
-      const password = "short1";
-      const response = await fetch(base_url + "user/register", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: email, password: password }),
-      });
-  
-      const data = await response.json();
-      expect(response.status).to.equal(400, data.error);
-      expect(data).to.be.an("object");
-      expect(data).to.include.all.keys("error");
-    });*/
-  });
+
+  }); //kvaak
